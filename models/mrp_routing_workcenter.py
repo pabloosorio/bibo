5607fbd2cf7a53a2eb57af90bc9766c02652e148
pabloosorio <35545995+pabloosorio@users.noreply.github.com>
2018-05-25 09:42:00 -0500

    Update mrp_routing_workcenter.py

--- models/mrp_routing_workcenter.py
+++ models/mrp_routing_workcenter.py
@@ -1,9 +1,6 @@
 from odoo import models, fields, api
 
 class MrpRoutingWorkorder(models.Model):
-    _inherit = 'mrp.routing.workcenter'
-    payment = fields.Float(string="Pago de empleado por pza.")
-
-class MrpWorkerCenter(models.Model):
-	_inherit = "mrp.workcenter"
-	mesa_default = fields.Many2one('board',string="Mesa por defecto")
+	_inherit = 'mrp.routing.workcenter'
+	payment = fields.Float(string="Pago de empleado por pza.")
+	mesa_defecto = fields.Many2one('board',string="Mesa por defecto")
